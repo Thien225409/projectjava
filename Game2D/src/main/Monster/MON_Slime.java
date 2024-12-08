7e893ca48af0a6f1e39120b51c92aaccaf6586d5
Bùi Khắc Anh <142425664+BKA121@users.noreply.github.com>
2024-12-08 10:45:21 +0700

    Update MON_Slime.java

--- Game2D/src/main/Monster/MON_Slime.java
+++ Game2D/src/main/Monster/MON_Slime.java
@@ -12,33 +12,35 @@ public class MON_Slime extends Entity{
         super(gp);
         this.gp = gp;
         
+        type =2;
         name = "Smile";
-        speed = 2;
-        maxLife = 4;
+        speed = 1;
+        maxLife = 1;
         life = maxLife;
+        
+        solidArea.x = 4;
+        solidArea.y = 11;
+        solidArea.width = 27;
+        solidArea.height = 15;
+        solidAreaDefaultX = solidArea.x;
+        solidAreaDefaultY = solidArea.y;
+        
+        getImage();
     }
 
     public void getImage(){
 
-        up1 = setup("/monster/slime1", gp.tileSize, gp.tileSize);
-        up2 = setup("/monster/slime1", gp.tileSize, gp.tileSize);
-        up3 = setup("/monster/slime1", gp.tileSize, gp.tileSize);
-        up4 = setup("/monster/slime1", gp.tileSize, gp.tileSize);
-
-        down1 = setup("/monster/slime2", gp.tileSize, gp.tileSize);
-        down2 = setup("/monster/slime2", gp.tileSize, gp.tileSize);
-        down3 = setup("/monster/slime2", gp.tileSize, gp.tileSize);
-        down4 = setup("/monster/slime2", gp.tileSize, gp.tileSize);
+        up1 = setup("/monster/slime_up1", gp.tileSize, gp.tileSize);
+        up2 = setup("/monster/slime_up2", gp.tileSize, gp.tileSize);
+      
+        down1 = setup("/monster/slime_down1", gp.tileSize, gp.tileSize);
+        down2 = setup("/monster/slime_down2", gp.tileSize, gp.tileSize);
         
-        right1 = setup("/monster/slime1", gp.tileSize, gp.tileSize);
-        right2 = setup("/monster/slime1", gp.tileSize, gp.tileSize);
-        right3 = setup("/monster/slime1", gp.tileSize, gp.tileSize);
-        right4 = setup("/monster/slime1", gp.tileSize, gp.tileSize);
+        right1 = setup("/monster/slime_right1", gp.tileSize, gp.tileSize);
+        right2 = setup("/monster/slime_right2", gp.tileSize, gp.tileSize);
     
-        left1 = setup("/monster/slime2", gp.tileSize, gp.tileSize);
-        left2 = setup("/monster/slime2", gp.tileSize, gp.tileSize);
-        left3 = setup("/monster/slime2", gp.tileSize, gp.tileSize);
-        left4 = setup("/monster/slime2", gp.tileSize, gp.tileSize);
+        left1 = setup("/monster/slime_left1", gp.tileSize, gp.tileSize);
+        left2 = setup("/monster/slime_left2", gp.tileSize, gp.tileSize);
     }
 
     public void setAction(){
@@ -49,16 +51,24 @@ public class MON_Slime extends Entity{
             int i = random.nextInt(100) + 1;// random a number from 1 to 100
 
             if(i <= 25 ){
-                direction = "up";
+            	if(0<worldY && worldY<70*gp.tileSize) {
+            		direction = "up";
+            	}
             }
             if(i > 25 && i <= 50){
-                direction = "down";
+            	if(0<worldY && worldY<70*gp.tileSize) {
+            		direction = "down";
+            	}                
             }
             if(i > 50 && i <= 75){
-                direction = "left";
+            	if(0<worldX && worldX<50*gp.tileSize) {
+            		direction = "left";
+            	}
             }
             if(i > 75){
-                direction = "right";
+            	if(0<worldX && worldX<50*gp.tileSize) {
+            		direction = "right";
+            	}
             }
 
             actionLockCounter = 0;
