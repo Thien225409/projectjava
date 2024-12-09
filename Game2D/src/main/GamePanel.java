b01d4485dac659850fb7c73e7e20eaab383a10ab
Bùi Khắc Anh <142425664+BKA121@users.noreply.github.com>
2024-12-09 23:17:05 +0700

    Update Projectile in GamePanel.java

--- Game2D/src/main/GamePanel.java
+++ Game2D/src/main/GamePanel.java
@@ -54,6 +54,7 @@ public class GamePanel extends JPanel implements Runnable {
     public Entity npc[] = new Entity[10];
     public Entity monster[] = new Entity[50];
     ArrayList<Entity> entityList = new ArrayList<>();
+    public ArrayList<Entity> projectileList = new ArrayList<>();
 
     //GAME STATE
     public int gameState;
@@ -133,6 +134,13 @@ public class GamePanel extends JPanel implements Runnable {
                     if(monster[i].alive == false) monster[i] = null;
                 }
             }
+            //MONSTER ATTACK
+            for(int i = 0; i < projectileList.size(); i++){
+                if(projectileList.get(i) != null){
+                    if(projectileList.get(i).alive == true ) projectileList.get(i).update();
+                    if(projectileList.get(i).alive == false) projectileList.remove(i);
+                }
+            }
 
         }
         if(gameState == pauseState){
@@ -177,6 +185,13 @@ public class GamePanel extends JPanel implements Runnable {
             for(int i = 0; i < monster.length; i++){
                 if(monster[i] != null){
                     entityList.add(monster[i]);
+                }
+            }
+
+            //MONSTER ATTACK
+            for(int i = 0; i < projectileList.size(); i++){
+                if(projectileList.get(i) != null){
+                    entityList.add(projectileList.get(i));
                 }
             }
 
